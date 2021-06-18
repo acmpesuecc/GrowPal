99ea662bfa1b1e91adf88e279397a60da5e48bcd
Anurag Rao <77329899+anuragrao04@users.noreply.github.com>
2021-06-18 22:15:59 +0530

    Update InitServer.sql

--- InitServer.sql
+++ InitServer.sql
@@ -15,23 +15,27 @@ PRIMARY KEY(username));
 
 CREATE TABLE credit_card_transactions(
 username VARCHAR(50),
+price VARCHAR(50),
 card_number VARCHAR(50),
 cvv VARCHAR(50),
 delivery_address VARCHAR(400));
 
 CREATE TABLE debit_card_transactions(
 username VARCHAR(50),
+price VARCHAR(50),
 card_number VARCHAR(50),
 cvv VARCHAR(50),
 delivery_address VARCHAR(400));
 
 CREATE TABLE upi_transactions(
 username VARCHAR(50),
+price VARCHAR(50),
 UPI_ID VARCHAR(50),
 delivery_address VARCHAR(400));
 
 CREATE TABLE net_bank_transactions(
 username VARCHAR(50),
+price VARCHAR(50),
 account_number VARCHAR(50),
 CIF_num VARCHAR(50),
 branch_code VARCHAR(50),
