6cd88e380f42c1ff248b910cc78a4a9a2d185944
anuragrao04 <raoanu2004@gmail.com>
2021-06-19 13:49:12 +0530

    Reverting to e1e6b17

--- InitServer.sql
+++ InitServer.sql
@@ -1,5 +1,4 @@
 CREATE USER 'admin_GrowPal'@'localhost' IDENTIFIED BY 'admin@password@GrowPal';
-DROP DATABASE IF EXISTS GrowPal;
 CREATE DATABASE GrowPal;
 GRANT ALL PRIVILEGES ON GrowPal.* TO 'admin_GrowPal'@'localhost';
 FLUSH PRIVILEGES;
@@ -19,20 +18,20 @@ username VARCHAR(50),
 price VARCHAR(50),
 card_number VARCHAR(50),
 cvv VARCHAR(50),
-flat_number VARCHAR(400));
+delivery_address VARCHAR(400));
 
 CREATE TABLE debit_card_transactions(
 username VARCHAR(50),
 price VARCHAR(50),
 card_number VARCHAR(50),
 cvv VARCHAR(50),
-flat_number VARCHAR(400));
+delivery_address VARCHAR(400));
 
 CREATE TABLE upi_transactions(
 username VARCHAR(50),
 price VARCHAR(50),
 UPI_ID VARCHAR(50),
-flat_number VARCHAR(400));
+delivery_address VARCHAR(400));
 
 CREATE TABLE net_bank_transactions(
 username VARCHAR(50),
@@ -40,4 +39,4 @@ price VARCHAR(50),
 account_number VARCHAR(50),
 CIF_num VARCHAR(50),
 branch_code VARCHAR(50),
-flat_number VARCHAR(400));
+delivery_address VARCHAR(400));
