335e9f96568a5ee278834d0108b9a815d5c4a4de
Anurag Rao <77329899+anuragrao04@users.noreply.github.com>
2022-01-15 18:45:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -73,9 +73,9 @@ product_price_listed = ''
 global product_description_listed
 product_description_listed = ''
 global db
-try: db = mysql.connector.connect(host='localhost', user = 'root', passwd = 'mysql_password', database = 'growpal')
+try: db = mysql.connector.connect(host='localhost', user = 'growpal_local', passwd = 'local_password', database = 'growpal')
 except:
-    try: db = mysql.connector.connect(host= 'archserver.ddns.net', user = 'developer', passwd = 'dev_password', database = 'growpal')
+    try: db = mysql.connector.connect(host= 'archserver.ddns.net', user = 'growpal', passwd = 'growpal4life', database = 'growpal')
     except: print("Error Connecting to SQL Server")
 
 try:
